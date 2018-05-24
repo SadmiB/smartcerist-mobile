f7fd6308c195247237dd5303ee28e91bfa94a96c
Tapa Autiomaan <dm_nait_belaid@esi.dz>
2018-05-24 09:10:26 +0100

    get user profile and notifications

---
+++ app/src/main/java/com/smartcerist/mobile/adapter/NotificationsCustomAdapter.java
@@ -0,0 +1,8 @@
+package com.smartcerist.mobile.adapter;
+
+/**
+ * Created by root on 17/05/18.
+ */
+
+public class NotificationsCustomAdapter {
+}
---
+++ app/src/main/java/com/smartcerist/mobile/model/Notification.java
@@ -0,0 +1,8 @@
+package com.smartcerist.mobile.model;
+
+/**
+ * Created by root on 18/05/18.
+ */
+
+public class Notification {
+}
